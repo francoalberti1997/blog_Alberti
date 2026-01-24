3490dcd884202dae5756aff94769cb17999e6657
Franco Alberti <albertifranco1997@hotmail.com>
2026-01-24 20:10:24 -0300

    new project

--- albertidl/urls.py
+++ albertidl/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
     path("ping/", views.ping_view, name="ping"),
+    path("create-superuser/", views.create_superuser_view, name="create_superuser"),
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
--- albertidl/views.py
+++ albertidl/views.py
@@ -2,3 +2,30 @@ from django.http import JsonResponse
 
 def ping_view(request):
     return JsonResponse({"message": "pong"})
+
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+
+User = get_user_model()
+
+def create_superuser_view(request):
+    username = "franco"
+    email = "franco@mail.com"
+    password = "franco"
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse(
+            {"status": "ok", "detail": "El superusuario ya existe"},
+            status=200
+        )
+
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+
+    return JsonResponse(
+        {"status": "created", "detail": "Superusuario creado correctamente"},
+        status=201
+    )
