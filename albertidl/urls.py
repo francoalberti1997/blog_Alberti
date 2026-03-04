090364e85f22497b5a01b2b4b37deba2f85b99c2
Franco Alberti <albertifranco1997@hotmail.com>
2026-03-04 04:24:55 -0300

    modelos app

--- albertidl/urls.py
+++ albertidl/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
+    path('modelos/', include('modelos.urls')),
     path("ping/", views.ping_view, name="ping"),
     path("create-superuser/", views.create_superuser_view, name="create_superuser"),
 
---
+++ request_test/micrografia.py
@@ -0,0 +1,35 @@
+import requests
+
+url = "http://localhost:8000/modelos/micrografias/"
+
+micrografias = [
+    {
+        "nombre": "Micro 100x",
+        "imagen": "imagenes/micro_1.jpg"
+    },
+    {
+        "nombre": "Micro 500x",
+        "imagen": "imagenes/micro_2.jpg"
+    },
+    {
+        "nombre": "Micro 1000x",
+        "imagen": "imagenes/micro_3.jpg"
+    }
+]
+
+for micro in micrografias:
+    data = {
+        "region": 1,  # ID de la región existente
+        "nombre": micro["nombre"]
+    }
+
+    files = {
+        "imagen": open(micro["imagen"], "rb")
+    }
+
+    response = requests.post(url, data=data, files=files)
+
+    print("Creando:", micro["nombre"])
+    print("Status:", response.status_code)
+    print(response.json())
+    print("-" * 40)
