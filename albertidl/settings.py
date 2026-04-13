14cd6ec57c164750c4874c88ad6477dd2e6fbace
Franco Alberti <albertifranco1997@hotmail.com>
2026-04-13 16:40:07 -0300

    set env variables

--- albertidl/settings.py
+++ albertidl/settings.py
@@ -10,23 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
-import os
 
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# ============================ CLOUDIFY ===============================
-
-import os
 import cloudinary
 
 cloudinary.config(
-    cloud_name="dtockjrdq",
-    api_key="186618873237985",
-    api_secret="KtbQqfUnhrbu1kwko4u3oGnTphA",
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
 )
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
